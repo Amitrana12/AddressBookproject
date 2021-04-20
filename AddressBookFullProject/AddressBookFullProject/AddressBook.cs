c7d5bc20b4624aa833ada4b88691d6046ce223e1
amit <[amirana14325@gmail.com]>
2021-04-20 18:52:18 +0530

    [Amit rana]Added::add multipal book without repreted

--- AddressBookFullProject/AddressBookFullProject/AddressBook.cs
+++ AddressBookFullProject/AddressBookFullProject/AddressBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AddressBookFullProject
@@ -16,64 +17,104 @@ namespace AddressBookFullProject
         /// </summary>
         public void AddContact(string first_name, string LastName, string address, string city, string state, int zip, long phone_number, string email)
         {
-            Contacts contact = new Contacts(first_name, LastName, address, city, state, zip, phone_number, email);
-            contactList.Add(contact);
-            Console.WriteLine("Thankyou!! (^_^) Contact added Successfully !\n--------------------------");
+            bool duplicate = equals(first_name);
+            if (!duplicate)
+            {
+                Contacts contact = new Contacts(first_name, LastName, address, city, state, zip, phone_number, email);
+                contactList.Add(contact);
+                Console.WriteLine("Contact added Successfully !\n---------------------");
+            }
+            else
+                Console.WriteLine("Cannot add duplicate Contact first name\n----------------");
         }
 
+        private bool equals(string first_name)
+        {
+            if (this.contactList.Any(e => e.first_name == first_name))
+                return true;
+            else
+                return false;
+        }
         public void EditContact(string first_name)
         {
-            foreach (Contacts contact in contactList)
+            if (contactList.Count() > 0)
             {
-                if (first_name.Equals(contact.first_name))
+                int thereExist = 1;
+                foreach (Contacts contact in contactList)
                 {
-                    Console.WriteLine("Enter Last Name : ");
-                    contact.last_name = Console.ReadLine();
-                    Console.WriteLine("Enter Address: ");
-                    contact.address = Console.ReadLine();
-                    Console.WriteLine("Enter City : ");
-                    contact.city = Console.ReadLine();
-                    Console.WriteLine("Enter State : ");
-                    contact.state = Console.ReadLine();
-                    Console.WriteLine("Enter Zip code : ");
-                    contact.zip = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Enter Phone Number : ");
-                    contact.phone_number = Convert.ToInt64(Console.ReadLine());
-                    Console.WriteLine("Enter Email : ");
-                    contact.email = Console.ReadLine();
-                    Console.WriteLine("Contact Updated Successfully !\n-----------------------------");
+                    if (first_name.Equals(contact.first_name))
+                    {
+                        thereExist = 0;
+                        Console.WriteLine("Enter Last Name : ");
+                        contact.last_name = Console.ReadLine();
+                        Console.WriteLine("Enter Address: ");
+                        contact.address = Console.ReadLine();
+                        Console.WriteLine("Enter City : ");
+                        contact.city = Console.ReadLine();
+                        Console.WriteLine("Enter State : ");
+                        contact.state = Console.ReadLine();
+                        Console.WriteLine("Enter Zip code : ");
+                        contact.zip = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Phone Number : ");
+                        contact.phone_number = Convert.ToInt64(Console.ReadLine());
+                        Console.WriteLine("Enter Email : ");
+                        contact.email = Console.ReadLine();
+                        Console.WriteLine("Contact Updated Successfully !\n-----------------------------");
+                    }
                 }
-                else
-                {
+                    if (thereExist == 1)
+                    {
                     Console.WriteLine($"Contact not found with first name '{first_name}'!\n------------------");
                 }
             }
-        }
+                else
+                {
+                    Console.WriteLine("This contact address book is empty. First add contact then try Editing");
+                }
+            }
         public void DeleteContact(string first_name)
         {
 
-            foreach (Contacts contact in contactList)
-            {
-                if (first_name.Equals(contact.first_name))
-                {
-                    contactList.Remove(contact);
-                    Console.WriteLine("Contact Deleted Successfully !\n----------------------------");
-                    break;
-                }
-                else
-                {
+            if (contactList.Count() > 0)
+            {               
+                
+                    int thereExist = 1;
+                    foreach (Contacts contact in contactList)
+                    {
+                        if (first_name.Equals(contact.first_name))
+                        {
+                            thereExist = 0;
+                            contactList.Remove(contact);
+                            Console.WriteLine("Contact Deleted Successfully !");
+                            break;
+                        }
+                    }
+                    if (thereExist == 1)
+                    {
                     Console.WriteLine($"Contact not found with first name '{first_name}'!\n----------");
                 }
             }
-        }
-        public void DisplayContacts()
-        {
-            foreach (Contacts contact in contactList)
-            {
-                Console.WriteLine($"First Name : {contact.first_name}\nLast Name : {contact.last_name}\nAddress : {contact.address}\nCity : {contact.city}\nState : {contact.state}\nZip : {contact.zip}\nPhone Number : {contact.phone_number}\nEmail : {contact.email}\n--------------------------");
-             
+                else
+                {
+                    Console.WriteLine("This contact address book is empty. First add contact then try Deleting");
+                }
             }
 
+
+
+        public void DisplayContacts()
+        {
+            if (contactList.Count() > 0)
+            {
+                foreach (Contacts contact in contactList)
+                {
+                    Console.WriteLine($"First Name : {contact.first_name}\nLast Name : {contact.last_name}\nAddress : {contact.address}\nCity : {contact.city}\nState : {contact.state}\nZip : {contact.zip}\nPhone Number : {contact.phone_number}\nEmail : {contact.email}\n--------------------------");
+                }
+            }
+            else
+            {
+                Console.WriteLine("This contact address book is empty. First add contact then try Displaying");
+            }
         }
     }
 }
