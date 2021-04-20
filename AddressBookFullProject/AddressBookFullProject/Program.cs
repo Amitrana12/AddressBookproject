4c93b45a4160fae627c77ec3526a3df1362033e5
amit <[amirana14325@gmail.com]>
2021-04-20 16:31:23 +0530

    [Amit rana]Added::Welcome massage

---
+++ AddressBookFullProject/AddressBookFullProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookFullProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcomr to my Address Book Full project by Amit Rana ");
+        }
+    }
+}
