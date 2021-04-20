9ef31e4b3d1863adf626c5bd2da57ec016ffa76e
amit <[amirana14325@gmail.com]>
2021-04-20 16:46:34 +0530

    [Amit rana]Added::Add contact into list

---
+++ AddressBookFullProject/AddressBookFullProject/AddressBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookFullProject
+{
+    class AddressBook
+    {
+        List<Contacts> contactList;
+        public AddressBook()
+        {
+            this.contactList = new List<Contacts>();
+        }
+        public void AddContact(string first_name, string LastName, string address, string city, string state, int zip, long phone_number, string email)
+        {
+            Contacts contact = new Contacts(first_name, LastName, address, city, state, zip, phone_number, email);
+            contactList.Add(contact);
+            Console.WriteLine("Thankyou!! (^_^) Contact added Successfully !");
+        }
+    }
+}
